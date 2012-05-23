27ac5fb4efc7be91f26778e92135d5afd70cd5ed
Vincent Siebert <vincent@siebert.im>
2012-05-24 00:08:09 +0100

    deploying deploy branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :user, "deploy"
 set :use_sudo, false
 ssh_options[:forward_agent] = true
 
+set :branch, "deploy"
 set :application, "cobblr"
 set :repository,  "git@github.com:thebeansgroup/cobblr.git"
 set :scm, :git
