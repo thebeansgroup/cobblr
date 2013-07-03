00b4c1d87238db0b9a6eeb4d81a1a75b8131e391
luxerama <vincent@siebert.im>
2013-07-03 16:39:54 +0100

    correcting link on index for voxburner generator

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :user, "deploy"
 set :use_sudo, false
 ssh_options[:forward_agent] = true
 
-set :branch, "master"
+set :branch, "feature/adding-voxburner"
 set :application, "cobblr"
 set :repository,  "git@github.com:thebeansgroup/cobblr.git"
 set :scm, :git
