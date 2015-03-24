ea36004a13cc042722eea828adc1f0aebdb75eb2
Ad Taylor <ad@adtaylor.co.uk>
2015-03-24 11:34:23 +0000

    Adds SBN footer

--- app.rb
+++ app.rb
@@ -21,6 +21,7 @@ end
 
 post '/display-tbg_email' do
   tbg
+  offices
   haml :tbg_email
 end
 
@@ -30,8 +31,20 @@ end
 
 post '/display-vxb_email' do
   vxb
+  offices
   haml :vxb_email
 end
+
+get "/sbn" do
+  haml :sbn_form
+end
+
+post '/display-sbn_email' do
+  sbn
+  offices
+  haml :sbn_email
+end
+
 
 def tbg
   @companyLogo = "http://tbg-assets.s3.amazonaws.com/thebeansgroup/email_footer/ef-TBG-logo.jpg"
@@ -55,4 +68,22 @@ def vxb
   @companyTwitter = "@voxburner"
   @companyTwitterURL = "http://twitter.com/voxburner"
   @companyLinkedin = "http://www.linkedin.com/company/voxburner"
+end
+
+def sbn
+  @companyLogo = "http://tbg-assets.s3.amazonaws.com/studentbeansnetwork/email_footer/SBN_logo.png"
+  @companyURL = "http://voxburner.com"
+  @companyColor = "#1D1D1B"
+  @companyName = "Student Beans Network"
+  @companyPhone = "0203 095 1400"
+  @companyWeb = "www.voxburner.com"
+  @companyTwitter = "@voxburner"
+  @companyTwitterURL = "http://twitter.com/voxburner"
+  @companyLinkedin = "http://www.linkedin.com/company/voxburner"
+end
+
+def offices
+  @office     = "Office address: Unit 420 Highgate Studios, 53-79 Highgate Road, London, NW5 1TL"
+  @registered = "Registered Office: 1 Vincent Square, London SW1P 2PN. Registered as a company in England and Wales, number: 5486885."
+  @vat        = "VAT Registration Number: 873 0262 33"
 end
